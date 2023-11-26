f46aec90fc295bf053403c8d1aac75713f0a9861
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-11-26 14:12:31 +0900

    chore: use same miniflare options as "wrangler dev"

--- app/miniflare.ts
+++ app/miniflare.ts
@@ -4,7 +4,12 @@ import { Miniflare } from "miniflare";
 const miniflare = new Miniflare({
   modules: true,
   script: `export default { fetch: () => new Response(null, { status: 404 }) }`,
-  kvNamespaces: ["kv"],
+  // mimic what `wranger dev` does, so that `pnpm preview` and `pnpm dev` shares same persistence
+  //   readConfig https://github.com/cloudflare/workers-sdk/blob/04a2d0ed6fca1c366cd891b54026c34e1c1a5701/packages/wrangler/src/config/index.ts#L28-L32
+  //   getBindings https://github.com/cloudflare/workers-sdk/blob/04a2d0ed6fca1c366cd891b54026c34e1c1a5701/packages/wrangler/src/dev.tsx#L856-L861
+  //   buildMiniflareOptions https://github.com/cloudflare/workers-sdk/blob/3b5407a968189e60974233c5db8615162db37fd2/packages/wrangler/src/dev/miniflare.ts#L497-L500
+  kvNamespaces: { kv: "583c3ad45f3c4baeb48e901e1cd8ad12" },
+  kvPersist: ".wrangler/state/v3/kv",
 });
 
 const env = await miniflare.getBindings();
