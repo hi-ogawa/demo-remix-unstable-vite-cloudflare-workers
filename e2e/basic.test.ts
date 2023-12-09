5dd24aac4e7d64eb7e4f1fa962bfdfd4eec92d72
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-09 11:43:02 +0900

    wip

--- e2e/basic.test.ts
+++ e2e/basic.test.ts
@@ -4,18 +4,18 @@ test("basic", async ({ page }) => {
   await page.goto("/kv", { waitUntil: "networkidle" });
   await page.getByTestId("hydrated").waitFor();
 
-  await page.getByText('counter = 0').click();
-  await page.getByRole('button', { name: '+1' }).click();
-  await page.getByText('counter = 1').click();
+  await page.getByText("counter = 0").click();
+  await page.getByRole("button", { name: "+1" }).click();
+  await page.getByText("counter = 1").click();
 
   await page.reload({ waitUntil: "networkidle" });
   await page.getByTestId("hydrated").waitFor();
-  await page.getByText('counter = 1').click();
+  await page.getByText("counter = 1").click();
 
-  await page.getByRole('button', { name: '-1' }).click();
-  await page.getByText('counter = 0').click();
-  await page.getByRole('button', { name: '-1' }).click();
-  await page.getByText('counter = -1').click();
-  await page.getByRole('button', { name: '+1' }).click();
-  await page.getByText('counter = 0').click();
+  await page.getByRole("button", { name: "-1" }).click();
+  await page.getByText("counter = 0").click();
+  await page.getByRole("button", { name: "-1" }).click();
+  await page.getByText("counter = -1").click();
+  await page.getByRole("button", { name: "+1" }).click();
+  await page.getByText("counter = 0").click();
 });
