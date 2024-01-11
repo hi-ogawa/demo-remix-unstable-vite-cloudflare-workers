2777bb0afc39e259c79a62648f627fad7e242ce8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-11 19:04:10 +0900

    chore: silence `optimizeDeps.include` warning (#19)

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,20 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   clearScreen: false,
-  plugins: [remix()],
+  plugins: [
+    remix(),
+    {
+      name: "local:exclude-remix-optimizeDeps",
+      enforce: "post",
+      config(config, _env) {
+        // silence warning since Remix currently forces @remix-run/node https://github.com/remix-run/remix/blob/ee1f202be9163b59f2eb4ba09ecc2bf10f17d401/packages/remix-dev/vite/plugin.ts#L674
+        //   Failed to resolve dependency: @remix-run/node, present in 'optimizeDeps.include'
+        if (config.optimizeDeps?.include) {
+          config.optimizeDeps.include = config.optimizeDeps.include.filter(
+            (v) => v !== "@remix-run/node",
+          );
+        }
+      },
+    },
+  ],
 });
