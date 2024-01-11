afb8c00984abdfc086fbc01716bd2ee910899e58
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-11 18:51:34 +0900

    refactor: simplify structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   clearScreen: false,
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [remix()],
 });
