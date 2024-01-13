2576a442be44324500f2fc73c2452ce50cce7a05
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-13 16:30:11 +0900

    chore: copy scripts

---
+++ misc/cloudflare-pages/README.md
@@ -0,0 +1,10 @@
+# cloudflare-pages
+
+(copied from https://github.com/hi-ogawa/remix-vite-deployment-examples/tree/main/misc/cloudflare-pages)
+
+script for cloudflare pages deployment
+
+```sh
+# initial setup
+npx wrangler pages project create remix-vite-example-hiro18181 --production-branch main
+```
---
+++ misc/cloudflare-pages/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu -o pipefail
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# static
+mkdir -p build/client
+cp -r ../../build/client/. build/client
+rm -rf build/client/.vite
+cp _headers _routes.json build/client
+
+# functions (cf. advanced mode https://developers.cloudflare.com/pages/platform/functions/advanced-mode/)
+npx esbuild ../../app/adapters/cloudflare-workers.ts \
+  --outfile=build/client/_worker.js \
+  --metafile=build/esbuild-metafile-cloudflare-pages.json \
+  --bundle --minify --format=esm --platform=browser
