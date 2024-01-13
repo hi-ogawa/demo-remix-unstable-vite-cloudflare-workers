89a7464c06a6182d727d8e172690a94f92f751bb
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-13 16:39:24 +0900

    chore: add scripts

--- misc/cloudflare-pages/README.md
+++ misc/cloudflare-pages/README.md
@@ -7,4 +7,11 @@ script for cloudflare pages deployment
 ```sh
 # initial setup
 npx wrangler pages project create remix-vite-example-hiro18181 --production-branch main
+
+# during local preview, wranger will use bindings in wrangler.toml
+pnpm pages-preview
+
+# before release, bindings must be setup manually from dashbaord
+# https://developers.cloudflare.com/pages/functions/bindings
+pnpm pages-release
 ```
--- misc/cloudflare-pages/build.sh
+++ misc/cloudflare-pages/build.sh
@@ -10,7 +10,7 @@ rm -rf build/client/.vite
 cp _headers _routes.json build/client
 
 # functions (cf. advanced mode https://developers.cloudflare.com/pages/platform/functions/advanced-mode/)
-npx esbuild ../../app/adapters/cloudflare-workers.ts \
+npx esbuild ../../app/server.mjs \
   --outfile=build/client/_worker.js \
   --metafile=build/esbuild-metafile-cloudflare-pages.json \
   --bundle --minify --format=esm --platform=browser
